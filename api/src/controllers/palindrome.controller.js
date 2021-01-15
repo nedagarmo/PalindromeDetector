426c361d623ba83f6f9bb46faadaee6e6642db47
Nelson Garzón <nedagarmo@gmail.com>
2021-01-15 15:55:03 -0500

    fix: retorno al encontrar un error en el parámtro text

--- api/src/controllers/palindrome.controller.js
+++ api/src/controllers/palindrome.controller.js
@@ -5,7 +5,7 @@ export default async function palindrome (req, res) {
 
     if (cleanParameter === '') {
       res.status(400).json({ text: 'no text' })
-      return;
+      return
     }
 
     const lowerCase = cleanParameter.toLowerCase()
