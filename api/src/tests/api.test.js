982ae2e8be4daa8fd85effb642bfbfc50f6d90d9
Nelson Garzón <nedagarmo@gmail.com>
2021-01-15 16:02:54 -0500

    fix: corrección de tests por el nuevo nombre de endpoint

--- api/src/tests/api.test.js
+++ api/src/tests/api.test.js
@@ -5,9 +5,9 @@ import server from '../server'
 const expect = chai.expect
 const request = supertest(server.app)
 
-describe('GET /palindrome', async () => {
+describe('GET /iecho', async () => {
   it('Debería retornar un código de estado HTTP 200 y palindrome en falso', async () => {
-    const response = await request.get('/palindrome?text=palabra')
+    const response = await request.get('/iecho?text=palabra')
 
     expect(response.status).to.eql(200)
     expect(response.body.text).to.eql('arbalap')
@@ -15,9 +15,9 @@ describe('GET /palindrome', async () => {
   })
 })
 
-describe('GET /palindrome', async () => {
+describe('GET /iecho', async () => {
   it('Debería retornar un código de estado HTTP 200 y palindrome en verdadero', async () => {
-    const response = await request.get('/palindrome?text=solos')
+    const response = await request.get('/iecho?text=solos')
 
     expect(response.status).to.eql(200)
     expect(response.body.text).to.eql('solos')
@@ -25,18 +25,18 @@ describe('GET /palindrome', async () => {
   })
 })
 
-describe('GET /palindrome', async () => {
+describe('GET /iecho', async () => {
   it('Debería retornar un código de estado HTTP 400 y la propiedad text igual a "no text"', async () => {
-    const response = await request.get('/palindrome')
+    const response = await request.get('/iecho')
 
     expect(response.status).to.eql(400)
     expect(response.body.text).to.eql('no text')
   })
 })
 
-describe('GET /palindrome', async () => {
+describe('GET /iecho', async () => {
   it('Debería retornar un código de estado HTTP 400 y la propiedad text igual a "no text"', async () => {
-    const response = await request.get('/palindrome?text=')
+    const response = await request.get('/iecho?text=')
 
     expect(response.status).to.eql(400)
     expect(response.body.text).to.eql('no text')
