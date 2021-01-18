f9c58c92eb2cdbb0d968bb671cd6c6e9d2f9641b
Nelson Garzón <nedagarmo@gmail.com>
2021-01-18 08:35:15 -0500

    fix: scrolling en la lista del historial de palíndromos

--- front/src/features/history/History.jsx
+++ front/src/features/history/History.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { list } from '../../reducers/palindromeSlice';
 
-import styles from './History.module.css'
+import styles from './History.css'
 
 const History = () => {
   const palindromeList = useSelector(list).slice().reverse().map((item, i) => {
@@ -12,7 +12,7 @@ const History = () => {
   return (
     <React.Fragment>
         <div className="container bg-white mt-5 p-5 h-100 mh-100">
-            <div className="row h-100 mh-100 scrolling">
+            <div className="row h-100 mh-100 scroll">
                 <div className="col-lg-12">
                     <h3>Results:</h3>
                     <div className="list-group w-75 mx-auto">
